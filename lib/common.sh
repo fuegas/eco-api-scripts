5e21fc459400945fb8625c187eb8a9462f11c26b
Ferdi van der Werf <ferdi@rootnet.nl>
2025-03-30 22:13:48 +0200

    Hide dump-headers behind --debug flag

--- lib/common.sh
+++ lib/common.sh
@@ -10,7 +10,7 @@ function error {
 server="${1-}"
 if [[ -z "${server}" ]]; then
   error "Server not passed as argument"
-  error "Usage: $0 <server ip:port>"
+  error "Usage: $0 <server ip:port> [flags]"
   exit 1
 fi
 status_path="${tmp_path}/${server}.status"
@@ -21,10 +21,19 @@ if [[ -t 1 ]]; then
   terminal=true
 fi
 
+# Do we want debug flags?
+debug=false
+if [[ "$@" =~ --debug ]]; then
+  debug=true
+fi
+
 # Base curl options on terminal state
-curl_opts='--silent'
-if $terminal; then
-  curl_opts='--dump-header /dev/stderr'
+curl_opts=''
+if ! $terminal; then
+  curl_opts="${curl_opts} --silent"
+fi
+if $debug; then
+  curl_opts="${curl_opts} --dump-header /dev/stderr"
 fi
 
 # Info will only output if you're running in a terminal
